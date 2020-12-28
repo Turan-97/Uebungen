d9ed746218b75dc2b969d949122b4a7afe12a456
Turan-97 <60237355+Turan-97@users.noreply.github.com>
2020-12-28 17:30:17 +0100

    Update heap_t.hpp

--- C++/heap_t/heap_t.hpp
+++ C++/heap_t/heap_t.hpp
@@ -1,153 +1,162 @@
-#include <iostream>
 #pragma once
+
+#include <iostream>
+
 using namespace std;
 
-template<class T>
-class heap_t 
+
+template <class T>
+class heap_t
 {
     private:
-
+        
         heap_t *head;
-        heap_t *next;
-        T value;
+        heap_t *next = NULL;
+        T value = 0;
 
 
     public:
 
-        heap_t() 
+        heap()
         {
             head = NULL;
         }
 
-        heap_t(int* values) 
+        heap(int *values)
         {
             head = NULL;
-            int i = 0;
 
-            while ((*(values + i)) != 99) 
-            {
-                insert(*(values + i));
-                i++;
-            }
+            for(int i = 0; i < 99; i++)
+                insert(*value);
         }
 
-    bool isEmpty();
+        bool isEmpty();
 
-    void insert(T value);
+        void insert(T value);
 
-    T minimum();
+        T minimum();
 
-    void extractMin();
+        void extractMin();
 
-    void print();
+        void printArray();
 
-    ~heap_t() 
-    {
-        delete head;
-        head = NULL;
-    }
+        ~heap()
+        {
+            delete head;
+        }
 };
 
 
-
 template <class T>
-bool heap_t<T>:: isEmpty() 
+bool heap_t<T>:: isEmpty()
 {
-    if(head == NULL) 
-    {
+    if(head == NULL)
         return true;
-    }
-    else 
-    {
+    else
         return false;
-    }
 }
 
 
-
 template <class T>
-void heap_t<T>:: insert(T value) 
+void heap_t<T>:: insert(T value)
 {
-    heap_t *newelement = new heap_t();
-    newelement->value = value;
+    heap_t *new_element = new heap_t;
+    new_element->value = value;
 
-    if(isEmpty() == true) 
+    if(isEmpty() == true)
     {
-        head = newelement;
+        head = new_element;
         return;
     }
-
-    heap_t* temp = head;
-
-    while(temp->next != NULL) 
-    {
-        temp = temp->next;
-    }
-
-    temp->next = newelement;
-}
-
-
-
-template <class T>
-void heap_t<T>:: print() 
-{
-    heap_t* temp = head;
     
-    while(temp != NULL) 
-    {
-        cout << temp->value << endl;
+    heap_t *temp = head;
+    
+    while(temp->next != NULL)
         temp = temp->next;
-    }
-}
 
+    temp->next = new_element;
+}
 
 
 template <class T>
-T heap_t<T>:: minimum() 
+T heap_t<T>:: minimum()
 {
-    try 
+    try
     {
-        heap_t* temp = head;
+        if(isEmpty() == true)
+            throw "Minimum existiert nicht, da der heap leer ist.\nProgramm wird beendet!\n";
 
-        if(temp == NULL) 
+        heap_t *temp = head;
+        T Min = temp->value;
+
+        while(temp->next != NULL)
         {
-            throw "Heap is empty";
+            temp = temp->next;
+            if(Min < temp)
+                Min = temp->value;
         }
 
-        return temp->value;
+        return Min;
 
-    }
-    catch(const char* err_string) 
+    }catch(const char *err)
     {
-        cerr << err_string << endl;
-        return 0;
-    }
-
+        cerr << err << endl;
+    };
 }
 
-template<class T>
 
-void heap_t<T>::extractMin() {
+template <class T>
+void heap_t<T>:: extractMin()
+{
+    try
+    {
+        if(isEmpty() == true)
+            throw "Minimum konnte nicht geloescht werden, da der heap leer ist.\nProgramm wird beendet!\n";
 
-    try {
+        delete minimum();
 
-        heap_t* temp = head;
+    }catch(const char *err)
+    {
+        cerr << err << endl;
+    };
+}
 
-        if (temp == NULL) {
-            throw "Heap is empty";
-            //return;
+
+template <class T>
+void heap_t<T>:: extractMin()
+{
+    try
+    {
+        if(isEmpty() == true)
+            throw "Minimum konnte nicht geloescht werden, da der heap leer ist.\nProgramm wird beendet!\n";
+
+        delete minimum();
+
+    }catch(const char *err)
+    {
+        cerr << err << endl;
+    };
+}
+
+
+template <class T>
+void heap_t<T>:: printArray()
+{
+    try
+    {
+        if(isEmpty() == true)
+            throw "heap ist leer.\nKeine Ausgabe moeglich.\n";
+
+        int n = 0;
+        heap_t *temp = head;
+
+        while(temp != NULL){
+            cout << temp->value << endl;
+            temp = temp->next;
         }
 
-        head = head->next;
-
-        delete temp;
-
-        temp = NULL;
-
-    }
-    catch (const char* err_string) 
+    }catch(const char *err)
     {
-        std::cerr << err_string << std::endl;
+        cerr << err << endl;
     };
 }
